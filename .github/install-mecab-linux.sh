7d727a43a2d92fa2f63cdfff3d0813e4a47042f7
Ichinose Shogo <shogo82148@gmail.com>
2020-02-14 21:11:17 +0900

    hello GitHub Actions

---
+++ .github/install-mecab-linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# from https://gist.github.com/dtan4/351d031bec0c3d45cd8f
+# see also http://qiita.com/dtan4/items/c6a087666296fbd5fffb
+
+set -uxe
+
+TMPDIR=$(mktemp -d)
+trap 'rm -rfv "$TMPDIR"' EXIT
+
+MECAB_VERSION=0.996.2
+
+# install mecab
+cd "$TMPDIR"
+curl -o mecab.tar.gz -sSL "https://github.com/shogo82148/mecab/releases/download/v$MECAB_VERSION/mecab-$MECAB_VERSION.tar.gz"
+tar zxfv mecab.tar.gz
+cd "mecab-$MECAB_VERSION"
+./configure --enable-utf8-only
+make
+make check
+sudo make install
+sudo ldconfig
+
+# install python binding
+cd "$TMPDIR"
+curl -o mecab-python.tar.gz -sSL "https://github.com/shogo82148/mecab/releases/download/v$MECAB_VERSION/mecab-python-$MECAB_VERSION.tar.gz"
+tar zxfv mecab-python.tar.gz
+cd "mecab-python-$MECAB_VERSION"
+python setup.py build
+python setup.py install
